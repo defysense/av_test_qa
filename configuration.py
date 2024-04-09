abb0868a3e42adb7df55a6e6bb2967d35c36515d
Quyen Ericka <quyeneri@student.21-school.ru>
2024-04-09 23:39:41 +0300

    11

--- configuration.py
+++ configuration.py
@@ -1,3 +1,4 @@
 SERVICE_URL = 'https://www.avito.ru/avito-care/eco-impact'
-
-# curl -X GET "https://www.avito.ru/avito-care/eco-impact" -H "accept: application/json"
+FULL_AREA = '.desktop-impact-items-F7T6E'
+ELEMENT_CSS_SELECTOR = '.desktop-impact-item-eeQO3'
+OUTPUT_DIR = 'output'
