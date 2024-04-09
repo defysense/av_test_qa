a442c622ad5371a6714162d47995fab30386bc3f
Quyen Ericka <quyeneri@student.21-school.ru>
2024-04-09 20:56:14 +0300

    work

--- configuration.py
+++ configuration.py
@@ -1,1 +1,3 @@
 SERVICE_URL = 'https://www.avito.ru/avito-care/eco-impact'
+
+# curl -X GET "https://www.avito.ru/avito-care/eco-impact" -H "accept: application/json"
--- src/task2/tests/test_1.py
+++ src/task2/tests/test_1.py
@@ -1,17 +1,48 @@
+import os
 import requests
 import sys
 sys.path.append('../..')
 from configuration import SERVICE_URL
+from src.enums.global_enums import GlobalErrorMessages
 
-def test_getting_card():
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'}
-    response = requests.get(url=SERVICE_URL, headers=headers)
-    assert response.status_code == 200, "Recieved status code is not equal to expected"
+from selenium import webdriver
+import pytest
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+@pytest.fixture(scope='module')
+
+def driver():
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+    yield driver
+    driver.quit()
+
+# def test_getting_card():
+#     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36', 'Accept': 'application/json'}
+#     response = requests.get(url=SERVICE_URL, headers=headers)
+#     # print(response.json())
+#     assert response.status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
+
+def test_screenshot(driver):
+    output_dir = 'output'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
     
-    # print(response.json())
-    # print(response.status_code)
-    # print(response.headers)
-    # json_data = json.loads(response.text)
-    # print(json_data)
-    # print(f'Total users: {response.json().get("total")}')
-    # print(requests.get(url='https://www.avito.ru/avito-care/eco-impact HTTP1.1/Content-Type:application/jsonAccept:application/json').content)
+    driver.get(url=SERVICE_URL)
+    element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.desktop-impact-items-F7T6E')))
+    element_full = driver.find_element(By.CSS_SELECTOR, '.desktop-impact-items-F7T6E')
+    ActionChains(driver).move_to_element(element).perform()
+    elements = driver.find_elements(By.CSS_SELECTOR, '.desktop-impact-item-eeQO3')
+    for i in range(1, 6, 2):
+        element = elements[i]
+        screenshot_file = os.path.join(output_dir, f'element_{i}.png')
+        element.screenshot(screenshot_file)
+    screenshot_file = os.path.join(output_dir, 'screenshot.png')
+    # element_full.screenshot(os.path.join(output_dir, 'element_full_screenshot.png'))
+    # element_co2.screenshot(os.path.join(output_dir, 'element_co2_screenshot.png'))
+    driver.save_screenshot(screenshot_file)
